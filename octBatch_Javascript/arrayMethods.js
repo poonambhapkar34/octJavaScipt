468e5c3b44e7ebd8d1d53dc42dd164f076c0a263
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-01-05 23:22:30 +0530

    regEx

--- octBatch_Javascript/arrayMethods.js
+++ octBatch_Javascript/arrayMethods.js
@@ -7,8 +7,9 @@ emp = [
 ]
 
 let data = emp.filter((element)=>{ 
-    //return  element.age == 50;
-  return  element.age == 50 && element.firstName == 'Raj1';
+    //return  element.age == 50; 37>35 && 37<=40
+    return  element.age >= 35 && element.age <= 40
+ // return  element.age == 50 && element.firstName == 'Raj1';
 })
 console.log("data",data);
 
@@ -18,3 +19,15 @@ emp.forEach(element => {
     newData.push(element.firstName)
 });
 console.log(newData);
+
+let a = [2,10,20,30,5];
+let c = 20;
+//  var d = a.map(mapFun());
+// function mapFun(arrayItems) {
+//    return arrayItems * 10;
+// }
+p = a.map( ele => ele * 10);
+console.log(p);
+
+var data1 = emp.map(xyz => xyz.firstName);
+console.log(data1);
---
+++ octBatch_Javascript/regex.js
@@ -0,0 +1,3 @@
+var text = " i live in mumbai , mumbai ,MUMbai,mumbai mm mmjllmmjjmm m ";
+console.log(text.match(/mm+/g));
+console.log(text.match(/mm*/g));
