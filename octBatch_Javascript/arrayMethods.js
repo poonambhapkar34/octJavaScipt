3c7746118e0441911f44553ce74d3363c9685a60
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-01-07 11:09:24 +0530

    map method

--- octBatch_Javascript/arrayMethods.js
+++ octBatch_Javascript/arrayMethods.js
@@ -1,10 +1,21 @@
+//Array Ittration methods :
 emp = [
     {firstName : 'Raj', lastName : "Patil", age:35},
     {firstName : 'Raj1', lastName : "Patil1", age:50},
     {firstName : 'Raj2', lastName : "Patil2", age:37},
-    {firstName : 'Raj3', lastName : "Patil1", age:50},,
+    {firstName : 'Raj3', lastName : "Patil3", age:50},
     {firstName : 'Raj1', lastName : "Patil1", age:50},
 ]
+var emp1 = emp.map(items=>{
+   return  empName = items.firstName;
+})
+console.log('emp1--->',emp1);
+
+const data1 = emp.map(xyz =>{
+    var newAge = xyz.age;
+  return newAge;
+})
+console.log('-->',data1);
 
 let data = emp.filter((element)=>{ 
     //return  element.age == 50; 37>35 && 37<=40
@@ -29,5 +40,39 @@ let c = 20;
 p = a.map( ele => ele * 10);
 console.log(p);
 
-var data1 = emp.map(xyz => xyz.firstName);
-console.log(data1);
+// const usersByLikes = myUsers.map(item => {
+//     var  container = item.likes;
+//     return container;
+// })
+//console.log(usersByLikes);
+
+
+
+
+const sweetArray = [2, 3, 4, 5, 35]
+const sweeterArray = sweetArray.map(sweetItem => {
+    // return sweetItem * 2
+    return sweetItem 
+})
+console.log(sweeterArray)
+
+
+const myUsers = [
+    { name: 'shark', likes: 'ocean',age:50 },
+    { name: 'turtle', likes: 'pond',age:50 },
+    { name: 'otter', likes: 'fish biscuits',age:50 },
+    { name: 'otte2r', likes: 'fish biscuits',age:50 },
+    { name: 'ottere', likes: 'fish biscuits',age:50 },
+    { name: 'otter3', likes: 'fish biscuits',age:50 }
+]
+const usersByLikes = myUsers.map(item => {
+ return  container = item.name;
+    //return container;
+})
+console.log("usersByLikes--->",usersByLikes);
+
+const data12 = myUsers.map(xyz =>{
+    var newAge = xyz.name;
+    return  newAge;
+})
+console.log('-->',data12);
