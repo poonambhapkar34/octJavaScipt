03d0fbeb7c5a1eeb6fb12489bafafe33843ede91
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-01-18 22:38:00 +0530

    interpolations

--- AngularProject/angularOctBatch/src/app/loginform/loginform.component.ts
+++ AngularProject/angularOctBatch/src/app/loginform/loginform.component.ts
@@ -5,12 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './loginform.component.html',
   styleUrls: ['./loginform.component.scss']
 })
+
 export class LoginformComponent {
+  car = 5100;
+  num = 20;
+  booleanProperty = false;
+  student = {
+    name:'jay',
+    age:20
+  }
+  items = [ 10, 20, 30, 40]
 
   keyUpEvent(data:any, inpBoxValue:any){
    console.log(data);
    console.log(data.target.value);
    console.log( inpBoxValue);
    console.log( inpBoxValue.value)
+  }
+
+  getData(b:any){
+  //  let a =20;
+    return 'i live in pune';
   }
 }
