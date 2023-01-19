7136b77a369bf6abe4be2929238872f13f346d0e
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-01-19 22:42:57 +0530

    one way data binding

--- AngularProject/angularOctBatch/src/app/loginform/loginform.component.ts
+++ AngularProject/angularOctBatch/src/app/loginform/loginform.component.ts
@@ -8,23 +8,29 @@ import { Component } from '@angular/core';
 
 export class LoginformComponent {
   car = 5100;
+  car1: any;
   num = 20;
   booleanProperty = false;
   student = {
-    name:'jay',
-    age:20
+    name: 'jay',
+    age: 20
   }
-  items = [ 10, 20, 30, 40]
+  items = [10, 20, 30, 40];
+  // myName :string = 'poonam patil'; //static data
+  myName: string = this.student.name; //dynamic data
+  disableFlag: boolean = false;
 
-  keyUpEvent(data:any, inpBoxValue:any){
-   console.log(data);
-   console.log(data.target.value);
-   console.log( inpBoxValue);
-   console.log( inpBoxValue.value)
+
+  keyUpEvent(data: any, inpBoxValue: any) {
+    console.log(data);
+    console.log(data.target.value);
+    console.log(inpBoxValue);
+    console.log(inpBoxValue.value)
+    this.car = 1000;
   }
 
-  getData(b:any){
-  //  let a =20;
+  getData(b: any) {
+    //  let a =20;
     return 'i live in pune';
   }
 }
