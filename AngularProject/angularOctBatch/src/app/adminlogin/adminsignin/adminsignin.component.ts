fbf7063b4813132aad72a3f7a05ed77bd7f4a29c
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-02-09 22:44:20 +0530

    reactive form

--- AngularProject/angularOctBatch/src/app/adminlogin/adminsignin/adminsignin.component.ts
+++ AngularProject/angularOctBatch/src/app/adminlogin/adminsignin/adminsignin.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// interface student{
+//   name:string,
+//   id:number,
+//   address:string
+// }
 @Component({
   selector: 'app-adminsignin',
   templateUrl: './adminsignin.component.html',
   styleUrls: ['./adminsignin.component.scss']
 })
+
+
 export class AdminsigninComponent {
   myFavourateColor = 'Black';
-  studentDataForm!: FormGroup;
+  studentDataForm!: FormGroup;//declaration of form
+  studentData : any = {
+    name:'pooja',
+    id:444,
+    address:'jjjj',
+  };
+  formData : any;
   constructor(public formBuilder: FormBuilder) {
    
   }
@@ -16,19 +29,30 @@ export class AdminsigninComponent {
     this.studentDataFormValidations();
 
   }
-
+//defination or controling of form
   studentDataFormValidations(){
    this.studentDataForm = this.formBuilder.group({
-    studentName : ['pooonam',Validators.required],
-    mobNo :[],
-    gender:[],
+    studentName : [this.studentData.name, [Validators.required,Validators.maxLength(5)]],
+    mobNo :[''],
+    gender:['male'],
     painting:[],
     dancing:[],
     reading:[],
     outing:[],
+    temsAndCond:['',[Validators.requiredTrue]],
+    dob:[]
    })
   }
-  submitFormData() {
-
+  submitFormData(value:any) {
+   this.formData = value;
+   console.log(value);
+   let date = this.formData.date?.split('-').reverse().join('-');
+   console.log(date);
+   let name1 =  this.formData.studentName.trim()
+   console.log(name1);
+   let name =  name1.toUpperCase();
+   console.log(name);
+   
+   
   }
 }
